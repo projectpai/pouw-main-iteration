bce72b8d9095eb1717420aac7f9cbd5fc012be19
awr-ai <andrei.lihu@aware.gg>
2021-09-08 23:47:23 +0200

    edge cases for pagination and fetching task details

--- pai/pouw/verification/task_info.py
+++ pai/pouw/verification/task_info.py
@@ -1,36 +1,9 @@
-import uuid
-import random
+import json
+
 import redis
 
 from pai.pouw.verification.task_info_pb2 import TaskListResponse, TaskRecord, Pagination, HTTPReturnCode
 from google.protobuf.timestamp_pb2 import Timestamp
-
-MODEL_TYPES = ['FC-DNN', 'CNN', 'LSTM', 'RNN']
-BATCH_SIZE = [16, 32, 64, 128, 256, 512, 1024]
-OPTIMIZERS = ['SGD', 'Adam', 'Nadam', 'Adagrad', 'Adamax']
-
-
-def get_tasks_info(redis_host='localhost', redis_port=6379):
-    conn = redis.Redis(host=redis_host, port=redis_port)
-    conn.ping()
-
-    training_tasks = list(set([task.decode("utf-8").split('_')[2] for task in conn.keys('training_start_*')]))
-    done_tasks = list(set([task.decode("utf-8").split('_')[2] for task in conn.keys('task_done_*')]))
-
-    final_submitted_tasks = []
-    final_training_tasks = []
-    final_done_tasks = []
-    for task in conn.scan_iter('task_submitted_*'):
-        task_id = task.decode("utf-8").split('_')[2]
-        if task_id in done_tasks:
-            final_done_tasks.append(task_id)
-            continue
-        if task_id in training_tasks:
-            final_training_tasks.append(task_id)
-            continue
-        final_submitted_tasks.append(task_id)
-
-    return [final_submitted_tasks, final_training_tasks, final_done_tasks]
 
 
 def get_waiting_tasks(page=1, per_page=20, redis_host='localhost', redis_port=6379):
@@ -41,7 +14,7 @@ def get_waiting_tasks(page=1, per_page=20, redis_host='localhost', redis_port=63
     result = [task.decode("utf-8").split('_')[2] for task in conn.scan_iter('task_submitted_*')
               if task.decode("utf-8").split('_')[2] not in training_tasks]
 
-    return get_mock_tasks(page, per_page)
+    return prepare_tasks_for_grpc(result, page, per_page)
 
 
 def get_started_tasks(page=1, per_page=20, redis_host='localhost', redis_port=6379):
@@ -53,7 +26,7 @@ def get_started_tasks(page=1, per_page=20, redis_host='localhost', redis_port=63
     result = [task.decode("utf-8").split('_')[2] for task in conn.scan_iter('training_start_*')
               if task.decode("utf-8").split('_')[2] not in done_tasks]
 
-    return get_mock_tasks(page, per_page)
+    return prepare_tasks_for_grpc(result, page, per_page)
 
 
 def get_completed_tasks(page=1, per_page=20, redis_host='localhost', redis_port=6379):
@@ -61,18 +34,41 @@ def get_completed_tasks(page=1, per_page=20, redis_host='localhost', redis_port=
     conn.ping()
 
     result = list(set([task.decode("utf-8").split('_')[2] for task in conn.keys('task_done_*')]))
-    return get_mock_tasks(page, per_page)
+    return prepare_tasks_for_grpc(result, page, per_page)
 
 
-def get_mock_tasks(page, per_page):
+def get_task_info(task_id, redis_host='localhost', redis_port=6379):
+    conn = redis.Redis(host=redis_host, port=redis_port)
+    conn.ping()
+
+    details = None
+    for full_task in conn.scan_iter('task_submitted_{}_*'.format(task_id)):
+        json_task_details = conn.mget(full_task.decode('utf-8'))
+        if json_task_details is not None and json_task_details[0] is not None:
+            details = json.loads(json_task_details[0].decode('utf-8'))
+        break
+
+    return details
+
+
+def prepare_tasks_for_grpc(task_ids, page=1, per_page=20, redis_host='localhost', redis_port=6379):
     task_list = []
     timestamp = Timestamp()
-    for i in range(random.randint(1, 30)):
+    for task_id in task_ids:
+        task_details = get_task_info(task_id, redis_host, redis_port)
         timestamp.GetCurrentTime()
-        task_record = TaskRecord(task_id=str(uuid.uuid4()), model_type=random.choice(MODEL_TYPES),
-                                 nodes_no=random.randint(1, 100) * 32,
-                                 batch_size=random.choice(BATCH_SIZE),
-                                 optimizer=random.choice(OPTIMIZERS), created=timestamp)
+        if task_details is not None:
+            task_record = TaskRecord(task_id=task_id, model_type=task_details['ml']['model']['type'],
+                                     nodes_no=sum([hu['nodes'] for hu in task_details['ml']['model']['hidden-units']]),
+                                     batch_size=task_details['ml']['optimizer']['batch-size'],
+                                     optimizer=task_details['ml']['optimizer']['type'],
+                                     created=timestamp)
+        else:
+            task_record = TaskRecord(task_id='', model_type='',
+                                     nodes_no=0,
+                                     batch_size=0,
+                                     optimizer='',
+                                     created=timestamp)
         task_list.append(task_record)
 
     # adjust for corner cases
@@ -80,13 +76,19 @@ def get_mock_tasks(page, per_page):
     if per_page <= 0 or per_page > task_list_len:
         per_page = task_list_len
 
-    total_pages = len(task_list) // per_page
-    if task_list_len % per_page != 0:
-        total_pages += 1
+    total_pages = 1
+    if per_page > 0:
+        total_pages = len(task_list) // per_page
+        if task_list_len % per_page != 0:
+            total_pages += 1
+
     if page <= 0:
         page = 1
-    elif page > total_pages:
+    elif page > total_pages and page != 1:
         page = total_pages
+
+    if total_pages == 0:
+        total_pages = 1
 
     navigation = Pagination.Navigation(self='page={}&per_page={}'.format(page, per_page),
                                        first='page=1&per_page={}'.format(per_page),
@@ -103,5 +105,5 @@ def get_mock_tasks(page, per_page):
 
 
 if __name__ == '__main__':
-    response = get_waiting_tasks()
+    response = get_started_tasks()
     print(1)
